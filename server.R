6de7fee49404719371f0b3d1c601ce6a1cf4b999
aplage <aplagegh@gmail.com>
2016-04-24 09:51:11 -0300

    Uploading the Shiny files

---
+++ server.R
@@ -0,0 +1,228 @@
+require(shiny)
+require(shinydashboard)
+require(dplyr)
+require(reshape2)
+require(ggplot2)
+
+surveysamplesize <- function(z = 0.975, p = 0.5, c = 0.05, pop = 20000) {
+                                samplesize1 <- ceiling(
+                                        (((qnorm(z)^2)*p*(1-p)) / c^2)
+                                )
+                                ifelse( samplesize1 < (0.1 * pop),
+                                        samplesize1,
+                                        ceiling(1 / ((1 / samplesize1) + (1/pop)))
+                                )
+                        }
+
+
+simp <- seq(0,1, 0.1)
+
+c10 <- 0.1
+c5 <- 0.05
+c1 <- 0.01
+simc <- c(c10, c5, c1)
+
+simnsize <- matrix(nrow = length(simc), ncol = length(simp))
+
+z90 <- 0.95
+z95 <- 0.975
+z99 <- 0.995
+simz <- c(z90, z95, z99)
+
+simnsizez <- matrix(nrow = length(simz), ncol = length(simp))
+
+simcolor <- c("black", "green4", "blue4")
+
+shinyServer(
+        function(input, output, session){
+                output$confidenceintervalBox <- renderValueBox({
+                        valueBox(
+                                input$conflevel, 
+                                "Confidence Level",
+                                icon = icon("percent"),
+                                color = "blue"
+                        )
+                })
+                output$marginoferrorBox <- renderValueBox({
+                        valueBox(
+                                input$me, 
+                                "Margin of Error", 
+                                icon = icon("percent"),
+                                color = "blue"
+                        )
+                })
+                output$expectedprevalenceBox <- renderValueBox({
+                        valueBox(
+                                input$expprev,
+                                "Expected Prevalence", 
+                                icon = icon("percent"),
+                                color = "blue"
+                        )
+                })
+                output$populationBox <- renderValueBox({
+                        valueBox(
+                                input$pop,
+                                "Population", 
+                                icon = icon("hashtag"),
+                                color = "blue"
+                        )
+                })
+                output$samplesizeBox <- renderValueBox({
+                        valueBox(
+                                surveysamplesize(z = 1 - ((1 - (input$conflevel) / 100)/2),
+                                                 p = input$expprev / 100,
+                                                 c = input$me / 100,
+                                                 pop = as.numeric(input$pop)),
+                                "Sample Size", 
+                                icon = icon("calculator"),
+                                color = "green"
+                        )
+                })
+                
+                output$distsamplesizeprevme <- renderPlot({
+                        
+                        sss <-surveysamplesize(z = 1 - ((1 - (input$conflevel) / 100)/2),
+                                               p = input$expprev / 100,
+                                               c = input$me / 100,
+                                               pop = as.numeric(input$pop))
+
+                        
+                        for(i in 1:length(simp)){
+                                for(j in 1:length(simz)){
+                                        simnsizez[j, i] <- surveysamplesize(z = simz[j],
+                                                                            p = simp[i], 
+                                                                            c = input$me / 100,
+                                                                            pop = as.numeric(input$pop))
+                                }
+                        }
+                        
+                        simnsizez <- as.data.frame(simnsizez)
+                        simnsizez <- cbind(simnsizez, simz)
+                        simnsizez2 <- melt(simnsizez, id.vars = "simz")
+                        simnsizez3 <- transmute(simnsizez2, simz = simz, variable = rep(simp, each = 3), value = value)
+
+                         
+                        plot(simnsizez3$variable, simnsizez3$value, 
+                             type = "n", 
+                             axes = FALSE, 
+                             ann = FALSE,
+                             ylim = c(min(simnsizez),
+                                      max(pretty(c(min(simnsizez),
+                                                   ifelse(max(simnsizez) < sss, sss, max(simnsizez))),
+                                                 10)))
+                        )
+                        for(i in seq_along(simz)){
+                                simnsizez3 %>% 
+                                        filter(simz == simz[i]) -> k
+                                points(k$variable, k$value, 
+                                       pch = 19, col = simcolor[i], cex = 1.2)
+                        }
+                        for(i in seq_along(simz)){
+                                simnsizez3 %>% 
+                                        filter(simz == simz[i]) -> k
+                                lines(k$variable, k$value, 
+                                      lwd = 2, col = simcolor[i])
+                        }
+                        points(input$expprev / 100, sss, pch = 19, cex = 1.5, col = "red")
+                        title(main = "Effects of Confidence Interval and Prevalence",  font.main = 2, 
+                              cex.main = 1.75, col.main = c("navyblue"), #title.adj = 0.5,
+                              xlab = "Prevalence",  font.lab = 2, cex.lab = 1.5,col.lab = c("blue"),
+                              ylab = "Sample Size",  font.lab = 2, cex.lab = 1.5,col.lab = c("blue"))
+                        axis(1, at = seq(0, 1, 0.1), 
+                             label = seq(0, 100, 10),
+                             font.axis = 2)
+                        axis(2, at = pretty(c(min(simnsizez),
+                                              ifelse(max(simnsizez) < sss, sss, max(simnsizez))),
+                                            10),
+                             label =pretty(c(min(simnsizez),
+                                             ifelse(max(simnsizez) < sss, sss, max(simnsizez))),
+                                           10),
+                             font.axis = 2)
+                        legend("topright",
+                               title = "Confidence Level",
+                               title.adj = -0.2,
+                               bty="n",  
+                               legend = c("90 %", "95 %","99 %", NA, "Calculated\nSample Size"),
+                               col = c(simcolor, NA, "red"),
+                               pch = c(rep(19,3), -1, 19),
+                               y.intersp = 0.25,
+                               cex = 0.7
+                        )
+                        
+                        
+                output$distsamplesizeprevcl <- renderPlot({
+                        
+                        sss <-surveysamplesize(z = 1 - ((1 - (input$conflevel) / 100)/2),
+                                           p = input$expprev / 100,
+                                           c = input$me / 100,
+                                           pop = as.numeric(input$pop))
+
+                        for(i in 1:length(simp)){
+                                for(j in 1:length(simc)){
+                                        simnsize[j, i] <- surveysamplesize(z = 1 - ((1 - (input$conflevel) / 100)/2), 
+                                                                           p = simp[i], 
+                                                                           c = simc[j], 
+                                                                           pop = as.numeric(input$pop))
+                                }
+                        }
+                        
+                        simnsize <- as.data.frame(simnsize)
+                        simnsize <- cbind(simnsize, simc)
+                        
+                        simnsize2 <- melt(simnsize, id.vars = "simc")
+                        simnsize3 <- transmute(simnsize2, simc = simc, variable = rep(simp, each = 3), value = value)
+                        
+                        plot(simnsize3$variable, simnsize3$value, 
+                             type = "n", 
+                             axes = FALSE, 
+                             ann = FALSE,
+                             ylim = c(min(simnsize),
+                                      max(pretty(c(min(simnsize),
+                                                   ifelse(max(simnsize) < sss, sss, max(simnsize))),
+                                                 10)))
+                        )
+                        for(i in seq_along(simc)){
+                                simnsize3 %>% 
+                                        filter(simc == simc[i]) -> k
+                                points(k$variable, k$value, 
+                                       pch = 19, col = simcolor[i], cex = 1.2)
+                        }
+                        for(i in seq_along(simc)){
+                                simnsize3 %>% 
+                                        filter(simc == simc[i]) -> k
+                                lines(k$variable, k$value, 
+                                      lwd = 2, col = simcolor[i])
+                        }
+                        points(input$expprev / 100, sss, pch = 19, cex = 1.5, col = "red")
+                        
+                        
+                        title(main = "Effects of Margin of Error and Prevalence",  font.main = 2, 
+                              cex.main = 1.75, col.main = c("navyblue"),# title.adj = 0.5,
+                              xlab = "Prevalence",  font.lab = 2, cex.lab = 1.5,col.lab = c("blue"),
+                              ylab = "Sample Size",  font.lab = 2, cex.lab = 1.5,col.lab = c("blue"))
+                        axis(1, at = seq(0, 1, 0.1), 
+                             label = seq(0, 100, 10),
+                             font.axis = 2)
+                        axis(2, at = pretty(c(min(simnsize),
+                                              ifelse(max(simnsize) < sss, sss, max(simnsize))),
+                                            10),
+                             label =pretty(c(min(simnsize),
+                                             ifelse(max(simnsize) < sss, sss, max(simnsize))),
+                                           10),
+                             font.axis = 2)
+                        legend("topright",
+                               title = "Margin of Error",
+                               title.adj = -0.2,
+                               bty="n",  
+                               legend = c("10 %", "5 %","1 %", NA, "Calculated\nSample Size"),
+                               col = c(simcolor, NA, "red"),
+                               pch = c(rep(19,3), -1, 19),
+                               y.intersp = 0.25,
+                               cex = 0.7
+                        )
+                        
+
+                })
+                })                
+        }
+)
---
+++ ui.R
@@ -0,0 +1,133 @@
+require(shiny)
+require(shinydashboard)
+
+shinyUI(
+        dashboardPage(
+                dashboardHeader(title = "Survey Sample Calculator", 
+                                titleWidth = 350),
+                dashboardSidebar(width = 350,
+                                 sidebarMenu(
+                                        menuItem("Calculator Inputs", tabName = "calculatorinputs"),
+                                                 sliderInput("conflevel", "Confidence Level (%)",  
+                                                             min = 90, max = 99.9, value = 95, step  = 0.5),
+                                                 sliderInput("me", "Margin of Error (%)",  
+                                                             min = 0.01, max = 10, value = 5, step  = 0.1),
+                                                 sliderInput("expprev", "Expected Prevalence (%)",  
+                                                             min = 0, max = 100, value = 50, step  = 0.5),
+                                                 textInput("pop", "Population Size", value = 10000
+                                                 ),
+                                        menuItem("Instructions", tabName = "instructions"),
+                                        menuItem("Formula", tabName = "formula"),
+                                        menuItem("References", tabName = "references")
+                                        )
+
+                                ),
+                dashboardBody(
+                        tabItems(
+                                tabItem("calculatorinputs",
+                                        fluidRow(
+                                                valueBoxOutput(width = 2,
+                                                               "confidenceintervalBox"),
+                                                valueBoxOutput(width = 2,
+                                                               "marginoferrorBox"),
+                                                valueBoxOutput(width = 2,
+                                                               "expectedprevalenceBox"),
+                                                valueBoxOutput(width = 2,
+                                                               "populationBox"),
+                                                valueBoxOutput(width = 4,
+                                                               "samplesizeBox")
+                                                 ),
+                                        fluidRow(
+                                                box(plotOutput("distsamplesizeprevme")),
+                                                box(plotOutput("distsamplesizeprevcl"))
+                                                )
+                                        ),
+                                tabItem("instructions",
+                                        fluidRow(
+                                                box(
+                                                        h2("Input panel"), 
+                                                        br(),
+                                                        h4("It has four widgets for input the parameters required for the calculations:"),
+                                                        br(),
+                                                        h3("Confidence Level slider"),
+                                                        h4("Adjust the confidence level required for estimating the sample size. It is the amount of certaint of the estimation. It is usually set to 95%, i.e, a Type I error rate of 5%. The slider let you adjust the confidence level from 90% to 99.9%."),
+                                                        h4("The default value is 95%."),
+                                                        h3("Margin of Error slider"),
+                                                        h4("This slider adjust the margin of error or precision of the estimate. The larger the margin of error you can tolerate, the smaller is the sample size. The slider let you adjust the margin of error from 0.01% to 10%."),
+                                                        h4("The default value is 5%."),
+                                                        h3("Expected Prevalence slider"),
+                                                        h4("In this slider you can set the expected prevalence. It let you adjust the prevalence from 0% to 100%."),
+                                                        h4("The default value is 50%, which results in the highest sample size estimate (See the graphics)."),
+                                                        h3("Population"),
+                                                        h4("In this box you can set the size of the population form which you are wihdrawing a sample. It is important for correcting the estimated sample for small populations (See the Formula tab)."),
+                                                        h4("The default value is 10000 individuals."),
+                                                        br(),
+                                                        h2("Results panel"), 
+                                                        br(),
+                                                        h3("Inputs"),
+                                                        h4("It has for blue boxes highlighting the input values."),
+                                                        br(),
+                                                        h3("Sample Size"),
+                                                        h4("A green box shows the estimated sample size for the input paramters."),
+                                                        br(),
+                                                        h3("Effect of Confidence Interval and Prevalance on Sample Size"),
+                                                        h4("This graphic shows the effect of Confidence Interval and Prevalance on Sample Size. It plots the estimate sample size for three common confidence levels, 90% (black), 95% (green) and 99% (blue), and prevalences from 0% to 100%, by 10%. The estimate sample size for the your input parameters is also plotted in red."),
+                                                        br(),
+                                                        h3("Effect of Margin of Error and Prevalance on Sample Size"),
+                                                        h4("This graphic shows the effect of Margin of Error and Prevalance on Sample Size. It plots the estimate sample size for three common margin of error, 10% (black), 5% (green) and 1% (blue), and prevalences from 0% to 100%, by 10%. The estimate sample size for the your input parameters is also plotted in red."),
+                                                        br(),
+                                                        h2("Formula Tab"),
+                                                        h4("It shows the formulae used for estimating the sample size."),
+                                                        br(),
+                                                        h2("References Tab"),
+                                                        h4("It shows the reference used for the calculations."),
+                                                        
+                                                        width = 12, status = "info", solidHeader = TRUE,
+                                                        title = "Instructions"
+                                                )
+                                        )
+                                        ),
+                                tabItem("formula",
+                                        fluidRow(withMathJax(),
+                                                box(
+                                                        h4("The survey sample is estimated based on the following formula: "),
+                                                        br(),
+                                                        h1("$$n = \\frac{Z^2p(1-p)}{L^2}$$"),
+                                                        br(),
+                                                        h4("where:"),
+                                                        br(),
+                                                        h4("n = the calculated survey sample size;"),
+                                                        h4("Z = the Z value required for the especified confidence;"),
+                                                        h4("p = the expected prevalence;"),
+                                                        h4("L = the margin of error, or precision, of the estimated sample size."),
+                                                        br(),
+                                                        h4("If the population is small, the Finite Population Correction is used based on the following formula:"),
+                                                        br(),
+                                                        h1("$$n_{corrected} = \\frac{1}{\\frac{1}{n} + \\frac{1}{pop}}$$"),
+                                                        br(),
+                                                        h4("where:"),
+                                                        br(),
+                                                        h4("n = the calculated survey sample size;"),
+                                                        h4("pop = the population size."),
+                                                        br(),
+                                                        h4("The Finite Population Correction is applied automatically if the estimated sample size is equal or greater then 10% of the population."),
+                                                        width = 12, status = "info", solidHeader = TRUE,
+                                                        title = "Formula")
+                                                )
+                                        ),
+                                tabItem("references",
+                                        fluidRow(
+                                                box(
+                                                        h3("DOHOO, I., MARTIN, W., STRYHN, H. 2003. Veterinary Epidemiologic Research. AVC Inc., p. 41."),
+                                                        width = 12, status = "info", solidHeader = TRUE,
+                                                        title = "References")
+                                        )
+                                )
+
+                                )
+                                
+                        )
+                        
+                )
+        )
+
